94857988b8dfc68ce6fafb773ed24290cb3fd85e
Chaiko85 <y.chaiko85@gmail.com>
2020-07-27 22:21:14 +0300

    Updates

---
+++ precache-manifest.0858b70708d9e78546a54075285ac08d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc752e5049391b04e9d21e7b01744a2",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "132f1d57d009460073e0",
+    "url": "/goit-react-hw-01-components/static/css/main.aebd6bfd.chunk.css"
+  },
+  {
+    "revision": "8d9dc58c1a4c85358da3",
+    "url": "/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132f1d57d009460073e0",
+    "url": "/goit-react-hw-01-components/static/js/main.63a1a14b.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
